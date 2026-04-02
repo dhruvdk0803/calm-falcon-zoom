6df435e3738ac74cbce4443684e5d5ce6d6bedf1
[dyad] <dhruvkhandelwal0803@gmail.com>
2026-04-02 16:50:58 +0530

    [dyad] Fixed Tailwind font configuration - wrote 1 file(s)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+  		fontFamily: {
+  			bebas: ['var(--font-bebas)', 'sans-serif'],
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
